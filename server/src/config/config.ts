44f24fedb69b65dbdec1f576fcab0db2d40c7287
gizemnur19 <gnkafadar@gmail.com>
2022-02-13 23:08:36 +0300

    config ts edited for heroku

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -2,5 +2,6 @@ export default {
   jwtSecret: "gizemkaf-secretkey",
   GOOGLE_CLIENT_ID:"569034342211-3kn0un2ikjmeug7qjviaebfmgq40se29.apps.googleusercontent.com",
   GOOGLE_CLIENT_SECRET:"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",
-  CLIENT_ADRESS:"http://localhost:3001"
+  CLIENT_ADRESS_FOR_LOCAL:"http://localhost:3001",
+  CLIENT_ADRESS:"https://gnk-movie-app.herokuapp.com"
 };
